ba5e88a0d6cfd659d117c1d5e333323459c7efc3
o0sh4d0w0o <25451887+o0sh4d0w0o@users.noreply.github.com>
2025-07-23 14:12:37 +0200

    Added project starter from start.spring.io with spring web for rest API

---
+++ src/main/java/fr/euodia/calctaxonproduct/CalctaxonproductApplication.java
@@ -0,0 +1,13 @@
+package fr.euodia.calctaxonproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalctaxonproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalctaxonproductApplication.class, args);
+	}
+
+}
